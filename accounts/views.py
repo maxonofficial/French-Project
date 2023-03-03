56c638552b426e49c48be7a051e6a8021f65da6f
Chitra-Banu <94032990+Chitra-Banu@users.noreply.github.com>
2023-03-03 12:19:13 +0530

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
-from django.urls import reverse_lazy
-from django_rest_passwordreset.views import PasswordResetView
-
-class CustomPasswordResetView(PasswordResetView):
-    email_template_name = 'email/password_reset_email.txt'
-    success_url = reverse_lazy('password_reset_done')
-
-
+# Create your views here.
